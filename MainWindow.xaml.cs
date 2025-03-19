dfaf5052f5dbbbb06dc4c212390b04b7636732ba
piringerm <piringerm@301-15>
2025-03-19 08:40:08 +0100

    a játék már viszonylag kész

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace krakout;
 
@@ -16,8 +10,41 @@ namespace krakout;
 /// </summary>
 public partial class MainWindow : Window
 {
+    double Xseb = 5;
+    double Yseb = 5;
+    int pontstam = 0;
     public MainWindow()
     {
         InitializeComponent();
+        var ido = new DispatcherTimer();
+        ido.Interval = TimeSpan.FromMilliseconds(1);
+        ido.Tick += Mozgatas;
+        ido.Start();
+    }
+
+    private void Mozgatas(object? sender, EventArgs e)
+    {
+        Canvas.SetLeft(jatekos, Mouse.GetPosition(jatekter).X);
+
+        var labdaY = Canvas.GetTop(labda);
+        var labdaX = Canvas.GetLeft(labda);
+        if (labdaX > 950) Xseb = -5;
+        if (labdaX < 0) Xseb = 5;
+        if (labdaY > 550) Yseb = -5;
+        if (labdaY < 0) Yseb = 5;
+
+        var jatekosX = Canvas.GetLeft(jatekos);
+        var jatekosY = Canvas.GetTop(jatekos);
+        if (labdaX+ labda.Width> jatekosX &&
+            labdaX< jatekosX +jatekos.Width&&
+            labdaY+ labda.Height > jatekosY&&
+            labdaY< jatekosY +jatekos.Height)
+        {
+            Yseb = -5;
+            lbpontszam.Content = ++pontstam;
+        }
+
+        Canvas.SetLeft(labda, labdaX + Xseb);
+        Canvas.SetTop (labda, labdaY + Xseb);
     }
 }
