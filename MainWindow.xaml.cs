8ebb0f7d5bf7b80463ad2f3a189cb1b629ff13ff
piringerm <piringerm@301-15>
2025-03-26 07:57:10 +0100

    a

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
 using System.Windows.Threading;
+using System.Windows.Media.Imaging;
 
 namespace krakout;
 
